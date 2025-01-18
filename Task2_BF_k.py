d821ffbccdf1999d8563d05e79215ebf66cb11ab
annavialykh <141519735+anfunctio@users.noreply.github.com>
2025-01-19 00:58:50 +0300

    Updated Task2_BF_k.py

--- Task2_BF_k.py
+++ Task2_BF_k.py
@@ -1,65 +1,66 @@
-import numpy as np
-import pandas as pd
 import mmh3
+import numpy as np
 from utils import gen_uniq_seq
+from tqdm import tqdm
+from bitarray import bitarray
 
 
 class BloomFilter_k:
     def __init__(self, k, n):
-        self.k = k
         self.n = n
-        self.array = np.zeros(self.n, dtype = int)
+        self.k = k
+        self.array = bitarray(n)
+        self.array.setall(0)
+        self.size_count = 0
 
-    def _hash(self, string):
-        hash_value = [mmh3.hash(string, i) % self.n for i in range(self.k)]
-        return hash_value
+    def _hash(self, string, seed):
+        return mmh3.hash(string, seed) % self.n
 
     def put(self, string):
-        for hash_value in self._hash(string):
-            self.array[hash_value] = True
+        for seed in range(self.k):
+            index = self._hash(string, seed)
+            if not self.array[index]:
+                self.array[index] = 1
+                self.size_count += 1
 
     def get(self, string):
-        return all(self.array[hash_value] for hash_value in self._hash(string))
+        for seed in range(self.k):
+            index = self._hash(string, seed)
+            if not self.array[index]:
+                return False
+        return True
 
     def size(self):
-        return np.count_nonzero(self.array) / self.k
-
-
-def generating_sequences(set_sizes):
-    for set_size in set_sizes:
-        gen_uniq_seq(str(set_size), set_size)
-
-
-def run_experiments_2(k, bf_sizes, set_sizes):
-    results = []
-
-    for _k in k:
-        for set_size in set_sizes:
-            for bf_size in bf_sizes:
-                bf = BloomFilter_k(k = _k, n = bf_size)
-            
-                fp_count = 0
-                with open(f'{set_size}') as file: 
-                    for s in file:
-                        if bf.get(s):
-                            fp_count += 1
-                        bf.put(s)
-                    ones_count = bf.size()
-
-                results.append({'k': _k,
-                                'bf_size': bf_size, 
-                                'set_size': set_size,
-                                'fp_count': fp_count,
-                                'ones_count': ones_count})
-    
-    return results
+        return self.size_count / self.k
 
 
 if __name__ == '__main__':
-    k = [1, 2, 3, 4]
     bf_sizes = [8, 64, 1024, 64000, 16000000]
     set_sizes = [5, 50, 500, 5000, 5000000]
+    k = [1, 2, 3, 4]
 
-    results = run_experiments_2(k, bf_sizes, set_sizes)
-    res2 = pd.DataFrame(results)
-    print(res2)
+    for set_size in set_sizes:
+        gen_uniq_seq(f'data_{set_size}.txt', set_size)
+    
+    results = []
+    for _k in tqdm(k, desc = 'Изменяем число хэш-функций'):
+      for set_size in set_sizes:
+        for bf_size in bf_sizes:
+          bf = BloomFilter_k(_k, bf_size)
+          fp_count = 0
+          
+          with open(f'data_{set_size}.txt', 'rt') as file:
+            for line in file:
+              if bf.get(line):
+                fp_count += 1
+              bf.put(line)
+            
+            ones_count = bf.size()
+            results.append((_k, bf_size, set_size, fp_count, ones_count))
+
+    print()
+    print('----------------------------------------------')
+    print('k | bf_size | set_size | fp_count | ones_count')
+    print('----------------------------------------------')
+    for result in results:
+      print(f'{result[0]} | {result[1]:<7} | {result[2]:<8} | {result[3]:<9} | {result[4]:.2f}')
