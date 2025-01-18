9ceb953f8305178785e1675a7522956308c5f9a5
annavialykh <141519735+anfunctio@users.noreply.github.com>
2025-01-19 00:57:59 +0300

    Updated Task1_BF_1.py

--- Task1_BF_1.py
+++ Task1_BF_1.py
@@ -1,12 +1,15 @@
 import numpy as np
-import pandas as pd
 import mmh3
+from bitarray import bitarray
 from utils import gen_uniq_seq
+
 
 class BloomFilter_1:
     def __init__(self, n):
         self.n = n
-        self.array = np.zeros(self.n, dtype = int)
+        self.array = bitarray(n)
+        self.array.setall(0)
+        self.size_count = 0
 
     def _hash(self, string):
         hash_value = mmh3.hash(string) % self.n
@@ -14,45 +17,42 @@ class BloomFilter_1:
 
     def put(self, string):
         index = self._hash(string)
-        self.array[index] += 1
+        if not self.array[index]:
+            self.array[index] = 1
+            self.size_count += 1
 
     def get(self, string):
         index = self._hash(string)
-        return self.array[index] > 0
+        return bool(self.array[index])
 
     def size(self):
-        return np.count_nonzero(self.array)
+        return self.size_count
 
-def generating_sequences(set_sizes):
-    for set_size in set_sizes:
-        gen_uniq_seq(str(set_size), set_size)
-
-def run_experiments(bf_sizes, set_sizes):
-    results = []
-    for set_size in set_sizes:
-        for bf_size in bf_sizes:
-            bf = BloomFilter_1(n = bf_size)
-        
-            fp_count = 0
-            with open(f'{set_size}') as file: 
-                for s in file:
-                    if bf.get(s):
-                        fp_count += 1
-                    bf.put(s)
-                ones_count = bf.size()
-
-            results.append({'bf_size': bf_size, 
-                            'set_size': set_size,
-                            'fp_count': fp_count,
-                            'ones_count': ones_count})
-    
-    return results
 
 if __name__ == '__main__':
-
     bf_sizes = [8, 64, 1024, 64000, 16000000]
     set_sizes = [5, 50, 500, 5000, 5000000]
 
-    results = run_experiments(bf_sizes, set_sizes)
-    res = pd.DataFrame(results)
-    print(res)
+    for set_size in set_sizes:
+        gen_uniq_seq(f'data_{set_size}.txt', set_size)
+    
+    results = []
+    for set_size in set_sizes:
+      for bf_size in bf_sizes:
+        fp_count = 0
+        bf = BloomFilter_1(bf_size)
+        with open(f'data_{set_size}.txt', 'rt') as file:
+          for line in file:
+            if bf.get(line):
+              fp_count += 1
+            bf.put(line)
+          
+          ones_count = bf.size()
+          results.append((bf_size, set_size, fp_count, ones_count))
+
+    print()
+    print('------------------------------------------')
+    print(' bf_size | set_size | fp_count  | ones_count')
+    print('------------------------------------------')
+    for result in results:
+      print(f'{result[0]:<8} | {result[1]:<8} | {result[2]:<9} | {result[3]}')
